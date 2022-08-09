d4713af5870b4d7147441b1434dd23e02dcfedf7
unknown <109669778+ChristianASC22@users.noreply.github.com>
2022-08-09 11:09:00 -0400

    first commit

--- firebase.js
+++ firebase.js
@@ -1,14 +1,22 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.9.2/firebase-app.js";
 import {
-  getDatabase,
-  ref,
-  push,
-  set,
-  onValue,
+  getDatabase,//gets my database
+  ref,//referance for specific parts of your database
+  push,//gnerates a unqiue number
+  set,//adds things to your database
+  onValue,//shows things in our database
 } from "https://www.gstatic.com/firebasejs/9.9.2/firebase-database.js";
 
 // Your web app's Firebase configuration
-// ENTER FIREBASE CONFIG HERE
+const firebaseConfig = {
+  apiKey: "AIzaSyCkUIPEgBX3MbuodmHAsmRrrZerdJL8bwo",
+  authDomain: "firesbasedemo-11ec6.firebaseapp.com",
+  projectId: "firesbasedemo-11ec6",
+  storageBucket: "firesbasedemo-11ec6.appspot.com",
+  messagingSenderId: "580793505547",
+  appId: "1:580793505547:web:945d48f80209a1ab5e740e",
+  measurementId: "G-4F7XQCT0WB"
+};
 
 
 // Initialize Firebase
--- main.js
+++ main.js
@@ -1,9 +1,39 @@
 import { ref, push, set, database, onValue } from "./firebase.js";
 
 //get elements from index.html
+let itemInput=document.getElementById("itemInput")
+let submitBtn=document.getElementById("submitBtn")
+let itemsContainer=document.getElementById("itemsContainer")
 
 //refrence a part of our database
+let itemsRef=ref(database,"items")
 
 //add item to database
+submitBtn.onclick= function(event){
+    event.preventDefault()
+    let itemInputVal=itemInput.value;
+    console.log(itemInputVal)
+    itemInput.value=""
+
+    let newItemRef=push(itemsRef)//a new place under the section items where we can place our data
+
+    set(newItemRef,itemInputVal)
+}
+
+
+
 
 //show items on firebase in our items container
+
+onValue(itemsRef, (snapshot)=>{
+    itemsContainer.innerHTML=""
+    let data=snapshot.val()
+    console.log(data)
+
+    for (const property in data){
+        console.log(data[property])
+        let newLi=document.createElement("li")
+        newLi.innerHTML=data[property]
+        itemsContainer.append(newLi)
+    }
+})
